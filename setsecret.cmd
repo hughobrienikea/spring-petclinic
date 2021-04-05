9396d3f62fc742d9fdaa50a72a4d894a1fdf0245
John Duimovich <jduimovich@gmail.com>
2021-04-05 11:02:11 -0400

    test it

--- setsecret.cmd
+++ setsecret.cmd
@@ -12,14 +12,29 @@ Set SERVER=%SERVERQ:"=%
 rem echo  The server is /%SERVER%/
 rem echo  The token is  /%LOGIN%/
  
+echo  Server: %SERVER%
+echo  Registry: quay.io
+echo.
+
 gh secret set OPENSHIFT_SANDBOX_URL -b %SERVER%
 gh secret set OPENSHIFT_SANDBOX_TOKEN -b %LOGIN%
  
 IF "%MY_QUAY_PW%"=="" (GOTO :missing_reg_token) 
 gh secret set REGISTRY_PASSWORD -b %MY_QUAY_PW%
+
+echo This app will be running on /%SERVER%/
+echo Workflow is configured to the following registry, user,namespace, app name and port 
+echo.
+yq r .github\workflows\petclinic-sample.yaml env.IMAGE_REGISTRY   
+yq r .github\workflows\petclinic-sample.yaml env.REGISTRY_USER  
+yq r .github\workflows\petclinic-sample.yaml env.TEST_NAMESPACE   
+yq r .github\workflows\petclinic-sample.yaml env.APP_NAME   
+yq r .github\workflows\petclinic-sample.yaml env.APP_PORT  
+
 goto :end
 
+
 :missing_reg_token
-echo missing github token in env var "MY_GITHUB_TOKEN" needed to install a runner
+echo missing REGISTRY_PASSWORD
 
 :end
