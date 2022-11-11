e85c646a53346e21c512e542850c8405b082f6b2
Hugh O'Brien <hugh.obrien1@inter.ikea.com>
2022-11-11 14:21:38 +0100

    updated

--- setsecret.cmd
+++ setsecret.cmd
@@ -13,14 +13,14 @@ rem echo  The server is /%SERVER%/
 rem echo  The token is  /%LOGIN%/
  
 echo  Server: %SERVER%
-echo  Registry: quay.io
+echo  Registry: docker.io
 echo.
 
 gh secret set OPENSHIFT_SANDBOX_URL -b %SERVER%
 gh secret set OPENSHIFT_SANDBOX_TOKEN -b %LOGIN%
  
-IF "%MY_QUAY_PW%"=="" (GOTO :missing_reg_token) 
-gh secret set REGISTRY_PASSWORD -b %MY_QUAY_PW%
+IF "%MY_DOCKER_PW%"=="" (GOTO :missing_reg_token) 
+gh secret set REGISTRY_PASSWORD -b %MY_DOCKER_PW%
 
 echo This app will be running on /%SERVER%/
 echo Workflow is configured to the following registry, user,namespace, app name and port 
